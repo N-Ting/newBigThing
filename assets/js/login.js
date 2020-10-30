1c66bacc2e933d9068b4aacdf7f8ae389a691321
nieting <1240057118@qq.com>
2020-10-30 18:15:53 +0800

    03

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,4 +9,18 @@ $(function () {
     $('.reg_box').hide()
     $('.login_box').show()
   })
+  // 自定义校验
+  var form = layui.form
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+      var pwd = $('.reg_box[name=password]').val()
+      if (pwd != value) {
+        return '两次密码不一致'
+      }
+    }
+  })
 })
