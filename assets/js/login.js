dca1ef3370545ca103a0bff8e927d1843e0fdda0
nieting <1240057118@qq.com>
2020-10-30 18:37:37 +0800

    04

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,10 +17,29 @@ $(function () {
       , '密码必须6到12位，且不能出现空格'
     ],
     repwd: function (value) {
-      var pwd = $('.reg_box[name=password]').val()
-      if (pwd != value) {
+      var pwd = $('.reg_box [name=password]').val()
+      // console.log(pwd);
+      if (pwd !== value) {
         return '两次密码不一致'
       }
     }
+  })
+
+  var BASE = 'http://ajax.frontend.itheima.net'
+  //监听注册表单的事件
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'post',
+      url: BASE + '/api/reguser',
+      data: {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val()
+      },
+      success: res => {
+        if (res.status !== 0) return console.log(res.message);
+        console.log('注册成功');
+      }
+    })
   })
 })
