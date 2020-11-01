5cd9c6316f249ba0c27e8f313906ef2c8e88a39a
nieting <1240057118@qq.com>
2020-11-01 18:28:09 +0800

    提交优化

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -8,14 +8,26 @@ $(function () {
       , '密码必须6到12位，且不能出现空格'
     ],
     newpwd: function (value) {
-      if (value === $('.layui-form [name=oldpwd]').val()) return '两次密码一样'
+      if (value === $('.layui-form [name=oldPwd]').val()) return '两次密码一样'
     },
     repwd: function (value) {
-      if (value !== $('.layui-form [name=repwd]').val()) return '两次密码不一致'
+      if (value !== $('.layui-form [name=newPwd]').val()) return '两次密码不一致'
     }
   })
   // 监听表单提交事件
   $('.layui-form').on('submit', function (e) {
     e.preventDefault()
+    // 获取ajax请求
+    $.ajax({
+      type: 'POST',
+      url: '/my/updatepwd',
+      data: $(this).serialize(),
+      success: res => {
+        if (res.status !== 0) return layer.msg('修改密码失败')
+        layer.msg('修改密码成功！')
+        // 清空表单
+        $('.layui-form')[0].reset()
+      }
+    })
   })
 })
